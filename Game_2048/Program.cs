9269cb437c090cd6c95cbe0f9360195b40ec0795
Renata Svikle <renata.svikle@inbox.lv>
2021-01-15 20:22:13 +0200

    First commit

---
+++ Game_2048/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Game_2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board newGame = new Board();
+            bool gameIsFinished = false;
+
+            newGame.newNumbers();                                            // method create a new number 
+
+            do // because we have to print the board at least once
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("New numbers!");                           // just for seeing that new numbers/number appear
+                newGame.newNumbers();                                        // method create a new number 
+                newGame.PrintBoard();                                        // and print board with new numbers
+                Console.WriteLine($"Score: {newGame.Score}");
+                Console.WriteLine();
+
+                Console.Write("For new move please press arrow: ");
+
+                switch (Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        newGame.newMoveRight();
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        newGame.newMoveLeft();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        newGame.newMoveUp();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        newGame.newMoveDown();
+                        break;
+                    default:
+                        gameIsFinished = true;
+                        break;
+                }
+
+            } while (!gameIsFinished);                                                  // "true" is temporary solution
+        }
+    }
+}
